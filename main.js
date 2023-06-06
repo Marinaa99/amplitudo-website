4bb1c83b7bca21f4a43383780563529551de528c
Marina <marina.vojinovic1313@gmail.com>
2023-06-06 13:56:57 +0200

    New changes

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ menuButton.addEventListener("click", () => {
   navList.classList.toggle("active");
 });
 
+const header = document.getElementById("header");
+window.addEventListener("scroll", function() {
+  const scrollPosition = window.scrollY;
+  
+  if (scrollPosition > 0) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
 let currentPageUrl = window.location.href;
 const navLinks = document.querySelectorAll(".nav-links li a");
 for (let i = 0; i < navLinks.length; i++) {
